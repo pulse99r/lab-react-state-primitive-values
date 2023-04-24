58b16280051ae34e19075ac2a4eb9226d1c9e863
Raymond <raymondjohnson@pursuit.org>
2023-04-24 17:57:24 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+  const [score, setScore] = useState(0);
+  const [payment, setPayment] = useState(1);
+  
+  function appReset(){
+    setScore(score - score )
+  }
+  function increaseScore(){
+    setScore(score + 1)
+    console.log("increasing score")
+  }
+  function payAmount(){
+    if(score >= 10){
+      //alert("Score is greater than 10!!")
+      // setPayment(payment + 10)
+      setScore(score + 10)
+    } else {
+      alert("You can't afford that!")
+    }
+  }
     return (
       <main>
-        <p>React State Lab</p>
+        <h1>Current Score: {score}</h1>
+        {score < 100 ? (
+          <>
+            <button onClick={ increaseScore } > +1 </button>
+            <button onClick={ payAmount}>Pay 10 points to change from {score} to {score + 2}</button>
+          </>
+          
+        ) : (
+          <>
+            <button onClick={appReset}>Play Again</button>
+            alert("You Win!!!")
+          </>
+        )}
       </main>
     );
 }
